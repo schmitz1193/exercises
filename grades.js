f78b7c47717f3439a3a8960798cc18a6b069076e
schmitz1193 <schmitz1193@gmail.com>
2015-10-13 11:58:53 -0500

    add grades files

---
+++ grades.js
@@ -0,0 +1,18 @@
+//Use console.log to output the following criteria:
+
+//How many of each grade?
+//What is the lowest grade?
+//What is the highest grade?
+
+
+
+
+
+
+
+
+
+
+
+
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
